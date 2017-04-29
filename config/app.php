27571352be752cb95a5bb0adeee895db8c0a3d81
belhard-user <ldarik2l@gmail.com>
2017-04-29 20:58:47 +0300

    routing

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * My package
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,38 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::any('/', function () {
+    $url = route('hello-page', [
+        'name' => 'neo',
+        'test' => 'foobar',
+        'bar' => 'asd',
+        'lname' => 'tank',
+    ]);
 
-Route::get('/', function () {
-    return view('welcome');
+    return "<a href='$url'>link</a>";
+});
+
+Route::name('hello-page')->get('hello/{name}-{lname}', function($name, $lname){
+    return "Hello " . ucwords(sprintf('%s %s', $name, $lname));
+})
+->where([
+    'name'=>'[a-z]+',
+    'lname' => '[a-z]+'
+]);
+
+
+Route::group(['prefix' => 'admin'], function($r){
+    /**
+     * @var \Illuminate\Routing\Router $r
+     */
+    $r->group(['prefix' => 'users'], function($r){
+        $r->get('{name}/edit', function($name){
+            return route('valera-page', ['name' => 'valera']);
+        })->name('valera-page');
+    });
+
+    $r->group(['prefix' => 'news'], function($r){
+        $r->get('{slug}/edit', function($slug){
+            return "Edit $slug article";
+        });
+    });
 });
