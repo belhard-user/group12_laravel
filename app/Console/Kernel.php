36e7c6e56ae1be4f03e1acb2fd4b1e9b35237493
belhard-user <ldarik2l@gmail.com>
2017-06-03 20:49:08 +0300

    console command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\JustDoit::class
     ];
 
     /**
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('just:doit')->everyMinute();
     }
 
     /**
